093a4d5b616855254136c5f1038f757c90e6f1c9
sshuai001 <2714192896@qq.com>
2026-06-04 18:33:44 +0800

    add pr triger in ci

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.use(express.static('dist'))
 
 const start = async () => {
---
+++ build_step.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Build script"
+
+# add the commands here
